3ce81b0d342d06d99a2a4d78d374977b450dc68c
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-10-05 20:15:29 +0530

    fix: Topbar organisation theme change

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link"
 import Image from "next/image"
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
 
 const Topbar = () => {
   return (
@@ -29,6 +30,7 @@ const Topbar = () => {
 
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4"
             }
