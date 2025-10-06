228b943737110b67eabb516101705274e91da02d
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-10-06 15:40:54 +0530

    fix: auth layout metadata fix

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,14 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "OutLoud",
+  description: "A Next.js 13 Meta social media app",
+};
 
 export default function RootLayout({
   children,
