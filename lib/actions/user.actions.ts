95d7c591547ae5fd1aa9bb7d7fddab00a3c970b4
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-10-07 22:05:49 +0530

    feat: user schema

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,46 @@
+'use server'
+
+import { connectToDB } from "../mongoose"
+import User from "../models/user.model";
+import { revalidatePath } from "next/cache";
+
+interface Params {
+  userId: string;
+  username: string;
+  name: string;
+  image: string;
+  bio: string;
+  path: string;
+}
+
+export const updateUserAction = async ({
+  userId,
+  username,
+  name,
+  image,
+  bio,
+  path
+}: Params): Promise<void> => {
+  
+  connectToDB();
+
+  try {
+    await User.findOneAndUpdate(
+      { id: userId },
+      { 
+        username: username.toLowerCase(),
+        name,
+        image,
+        bio,
+        onboarded: true
+      },
+      { upsert: true }
+    );
+
+    if (path === "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    throw new Error("Failed to create/update user: " + error.message);
+  }
+}
