df2fae3890253007991258c52e82c036e4ed63c6
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-10-09 22:08:31 +0530

    feat: create-thread page layout

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,29 @@
+
+import PostThread from "@/components/forms/PostThread";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+
+const Page = async () => {
+
+  const user = await currentUser();
+
+  if (!user) {
+    redirect('/sign-in')
+  }
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) {
+    redirect('/onboarding')
+  }
+
+  return (
+    <>
+      <h1 className="head-text">Create thread</h1>
+      <PostThread userId={userInfo._id} />
+    </>
+  )
+}
+
+export default Page
