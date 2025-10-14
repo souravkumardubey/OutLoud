16271aa9b657cd50403dcad5bdc2808336f2aebf
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-10-15 01:50:49 +0530

    fix: comment validation

--- lib/validations/thread.ts
+++ lib/validations/thread.ts
@@ -6,7 +6,7 @@ export const ThreadValidation = z.object({
 });
 
 export const CommentValidation = z.object({
-  comment: z.string().nonempty().min(1),
-  accountId: z.string(),
-  threadId: z.string(),
+  thread: z.string().nonempty().min(3, {
+    message: "Minimum 3 characters."
+  }),
 });
