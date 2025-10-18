1d707187268f3b5ce0e968e7a133841d7f0042dd
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-10-18 23:52:58 +0530

    fix: comment card

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -102,7 +102,7 @@ const ThreadCard = ({
                   className="cursor-pointer object-contain"
                 />
               </div>
-              {isComment && comments.length > 0 && (
+              {comments.length > 0 && (
                 <Link href={`/thread/${id}`}>
                   <p className="mt-1 text-subtle-medium text-gray-1">
                     {comment.length} replies
