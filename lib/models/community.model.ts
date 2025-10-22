1671afcf24e5b33c75cd34ac5643617686be6521
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-10-22 13:52:01 +0530

    fix(schema): add threads field to Community model for thread population
    - Added  field referencing the Thread model in Community schema
    - Enables proper population of community posts in fetchCommunityPosts
    - Fixed populate error caused by missing schema reference

--- lib/models/community.model.ts
+++ lib/models/community.model.ts
@@ -10,6 +10,12 @@ const communitySchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   },
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread"
+    }
+  ],
   members: [
     {
       type: mongoose.Schema.Types.ObjectId,
